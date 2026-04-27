2c5f1feafd547e08ac11867bc24c9d082a3a5f5b
Cobwebsticky <cobweb.sticky@gmail.com>
2026-04-27 15:17:02 +0300

    Add frontend-deploy for Render

---
+++ frontend-deploy/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'admin.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Frontend running on port ${port}`);
+});
